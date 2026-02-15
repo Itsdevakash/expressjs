08fa8d947509d7739e1ae1898c3bf76f34bea823
Itsdevakash <mauryaakash342@gmail.com>
2026-02-16 00:01:36 +0530

    done render html

--- express-second (ES Module)/renderHtml.js
+++ express-second (ES Module)/renderHtml.js
@@ -1,12 +1,35 @@
 import express from "express"
+import lohin from "./pages/login.js";
 const app =express();
  
 
 app.get('/',(req,resp)=>{
 
-resp.send("<h1>Html</h1>");
+resp.send("<h1>Home Page </h1> <br> <a href='login'>Go to Login </a>");
 
 })
+
+
+
+app.get('/login',(req,resp)=>{
+
+resp.send(lohin());
+
+})
+
+
+
+
+
+
+
+app.post('/submit',(req,resp)=>{
+
+resp.send("<h1>DataSubmit</h1> <br>    <a href='/'>Go to Home</a>");
+
+})
+
+
 
 
 app.listen(3800);
